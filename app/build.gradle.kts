f3933e355cdbc8cc0a859341a212fc3a5c85f3ee
Iain Forrest <iainforrest@me.com>
2025-06-14 15:34:08 +1200

    Bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.editecho"
         minSdk        = 31
         targetSdk     = 34
-        versionCode   = 2
-        versionName   = "0.2.0"
+        versionCode   = 3
+        versionName   = "0.3.0"
 
         buildConfigField(
             "String",
